93dd66426d2a12513486a0376d4d978b77f2227a
Nicolas Lecoy <nicolas.lecoy@gmail.com>
2020-11-17 11:42:43 +0100

    ref: Add default value for consumer type and factory class

--- README.md
+++ README.md
@@ -20,11 +20,8 @@ The rest of the configuration properties for your stream should be set with the 
 Here is a list of available configurations for this plugin.
 Remember, due to the lack of low level stream type, some configurations have to be set with default values (See below):
 
-| Parameter                                | Description                                    | Default                                     |
-|:-----------------------------------------|:-----------------------------------------------|:--------------------------------------------|
-| `stream.pubsub.gcpKeyPath`               | Path to GCS service account key file           |                                             |
-| `stream.pubsub.projectId`                | GCP project ID of the Pub/Sub to be read       |                                             |
-| `stream.pubsub.subscriptionId`           | Subscription ID of the Pub/Sub to be read      |                                             |
-| `stream.pubsub.topicName`                | Topic name of the Pub/Sub to be read           |                                             |
-| `stream.pubsub.consumer.type`            | Type of the Stream Plugin                      | `HighLevel`                                 |
-| `stream.foo.consumer.factory.class.name` | Fully qualified consummer class implementation | `com.reelevant.pinot.PubSubConsumerFactory` |
+| Parameter                                   | Description                                    |
+|:--------------------------------------------|:-----------------------------------------------|
+| `stream.pubsub.projectId`                   | GCP project ID of the Pub/Sub to be read       |
+| `stream.pubsub.subscriptionId`              | Subscription ID of the Pub/Sub to be read      |
+| `stream.pubsub.topicName`                   | Topic name of the Pub/Sub to be read           |
--- src/main/java/com/reelevant/pinot/PubSubStreamLevelStreamConfig.java
+++ src/main/java/com/reelevant/pinot/PubSubStreamLevelStreamConfig.java
@@ -14,14 +14,11 @@ public class PubSubStreamLevelStreamConfig {
 	private final String topicName;
 	private final Map<String, String> pubSubConsumerProperties;
 
-	public PubSubStreamLevelStreamConfig(StreamConfig streamConfig, String projectId, String subscriptionId, String topicName) {
+	//public PubSubStreamLevelStreamConfig(StreamConfig streamConfig, String projectId, String subscriptionId, String topicName) {
+	public PubSubStreamLevelStreamConfig(StreamConfig streamConfig, String tableName, String groupId) {
 		Map<String, String> streamConfigMap = streamConfig.getStreamConfigsMap();
 
-		this.projectId = projectId;
-		this.subscriptionId = subscriptionId;
-		this.topicName = topicName;
 		this.pubSubConsumerProperties = new HashMap<>();
-
 		String pubSubConsumerPropertyPrefix = PubSubStreamConfigProperties.constructStreamProperty(PubSubStreamConfigProperties.PUBSUB_CONSUMER_PROP_PREFIX);
 		for (String key: streamConfigMap.keySet()) {
 			if (key.startsWith(pubSubConsumerPropertyPrefix)) {
@@ -29,18 +26,10 @@ public class PubSubStreamLevelStreamConfig {
 					.put(StreamConfigProperties.getPropertySuffix(key, pubSubConsumerPropertyPrefix), streamConfigMap.get(key));
 			}
 		}
-	}
 
-	public String getProjectId() {
-		return this.projectId;
-	}
-
-	public String getSubscriptionId() {
-		return this.subscriptionId;
-	}
-
-	public String getTopicName() {
-		return this.topicName;
+		this.projectId = this.pubSubConsumerProperties.get("projectId");
+		this.subscriptionId = this.pubSubConsumerProperties.get("subscriptionId");
+		this.topicName = this.pubSubConsumerProperties.get("topicName");
 	}
 
 	public Properties getPubSubConsumerProperties() {
@@ -56,8 +45,22 @@ public class PubSubStreamLevelStreamConfig {
 		return props;
 	}
 
+	public String getProjectId() {
+		return this.projectId;
+	}
+
+	public String getSubscriptionId() {
+		return this.subscriptionId;
+	}
+
+	public String getTopicName() {
+		return this.topicName;
+	}
+
 	static {
-		// Default PubSub properties should be defined here.
+		// Default PubSub properties (if any) should be defined here.
 		defaultProps = new HashMap<>();
+		defaultProps.put("stream.pubsub.consumer.type", "HighLevel");
+		defaultProps.put("stream.pubsub.consumer.factory.class.name", "com.reelevant.pinot.PubSubConsumerFactory");
 	}
 }
